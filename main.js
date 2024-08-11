fcddd099fa10638f561731bfc32674a3f9a0e6bf
Emacreator <Ronald_7_9@hotmail.com>
2024-08-11 12:11:12 -0300

    agregue ejercicios

---
+++ main.js
@@ -0,0 +1,65 @@
+//1- Crear una función que reciba un número por parámetro e indique en consola si el número es par o impar.
+
+//const num = 4;
+//if (num % 2 === 0) {
+//console.log("Es par");
+//} else {
+//console.log("Es impar");
+//}
+
+//2- Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales.
+
+//ver
+
+//function mayorMenor(num1, num2) {
+//if (num1 > num2) {
+//console.log(`El mayor es ${num1}`);
+//} else if (num1 < num2) {
+//console.log(`El menor es ${num2}`);
+//} else {
+//console.log(`los numeros son iguales`);
+//}
+//}
+
+//mayorMenor(1);
+//mayorMenor(12);
+
+//3- Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5.
+
+//ver
+
+//function multiploDe5(num) {
+//if (num * 5) {
+//console.log(`El numero, ${num} es multiplo de 5`);
+//} else {
+//console.log("El numero no es multiplo de 5");
+//}
+//}
+
+//multiploDe5(17);
+
+//4- Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.
+
+//const imprimirNumero = (num) => {
+//for (let i = 0; i <= num; i++) {
+//console.log(i);
+//}
+//};
+
+//imprimirNumero(5);
+
+//5 - Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado.
+
+const imprimirLetrayNumero = (num) => {
+  for (let i = 0; i < num; i++) {
+    console.log("Texto a Imprimir");
+  }
+};
+
+imprimirLetrayNumero(20);
+
+//6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.
+
+//7 - Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0".
+
+//8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
