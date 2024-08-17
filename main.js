0d4bd97365ea4832f48207dd2958a06b6bc50e55
Emacreator <Ronald_7_9@hotmail.com>
2024-08-17 14:40:12 -0300

    nuevos ejercicios resueltos

--- main.js
+++ main.js
@@ -9,34 +9,29 @@
 
 //2- Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales.
 
-//ver
+// function mayorMenor(num1, num2) {
+//   if (num1 > num2) {
+//     console.log(`El mayor es ${num1}`);
+//   } else if (num1 < num2) {
+//     console.log(`El menor es ${num2}`);
+//   } else {
+//     console.log(`Los numeros son iguales`);
+//   }
+// }
 
-//function mayorMenor(num1, num2) {
-//if (num1 > num2) {
-//console.log(`El mayor es ${num1}`);
-//} else if (num1 < num2) {
-//console.log(`El menor es ${num2}`);
-//} else {
-//console.log(`los numeros son iguales`);
-//}
-//}
-
-//mayorMenor(1);
-//mayorMenor(12);
+// mayorMenor(30, 122);
 
 //3- Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5.
 
-//ver
+// function multiploDe5(num) {
+//   if (num > 0 && num % 5 == 0) {
+//     console.log(`El numero, ${num} es multiplo de 5`);
+//   } else {
+//     console.log(`El numero, ${num} no es multiplo de 5`);
+//   }
+// }
 
-//function multiploDe5(num) {
-//if (num * 5) {
-//console.log(`El numero, ${num} es multiplo de 5`);
-//} else {
-//console.log("El numero no es multiplo de 5");
-//}
-//}
-
-//multiploDe5(17);
+// multiploDe5(50);
 
 //4- Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.
 
@@ -54,12 +49,43 @@
 //for (let i = 0; i < num; i++) {
 //console.log("Texto a Imprimir");
 //}
-//};
 
 //imprimirLetrayNumero(20);
 
 //6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.
 
+// const Jugador = [
+//   "Martínez",
+//   "Montiel",
+//   "L.Martinez",
+//   "Tagliafico",
+//   "Romero",
+//   "Fernandez",
+//   "MacAllister",
+//   "De Paul",
+//   "Di María",
+//   "Álvarez",
+//   "Messi",
+// ];
+
+// for (let i = 0; i < Jugador.length; i++) {
+//   console.log(Jugador[i]);
+// }
+
 //7 - Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0".
 
+// const numeros = ["23", "4", "25", "3", "13", "24", "20", "7", "11", "10"];
+
+// for (let i = 0; i < numeros.length; i++) {
+//   if (i !== 4) {
+//     console.log(numeros[i]);
+//   }
+// }
+
 //8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
+
+// const Numeros = [23, 4, 25, 3, 13, 24, 20, 7, 11, 10];
+
+// for (let multiplicar of Numeros) {
+//   console.log(multiplicar * 4);
+// }
